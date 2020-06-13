e35c181957127d86da4803e1abc22f7111f483a5
Ajay Asok <ajayasok007@gmail.com>
2020-06-14 02:16:42 +0530

    route method implementation changes

--- express-dev/index.js
+++ express-dev/index.js
@@ -3,7 +3,27 @@ import data from './data/data.json'
 
 const app = express()
 const PORT = 3000
+// Get sample
+app.get('/', (req, res) => {
+    res.send(`Get request Served at ${PORT}`)
+})
 
+// Post sample
+app.post('/', (req, res) => {
+    res.send(`Post request Served at ${PORT}`)
+})
+
+// Put sample
+app.put('/', (req, res) => {
+    res.send(`Put request Served at ${PORT}`)
+})
+
+// Delete Sample
+app.delete('/', (req, res) => {
+    res.send(`Delete request Served at ${PORT}`)
+})
+
+//Delete sample
 app.listen(PORT, () => {
     console.log(`Server starts and running on ${PORT}`)
     console.log(data)
